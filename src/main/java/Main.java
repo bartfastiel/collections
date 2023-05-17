2203594b0eee53022053204a2186186210c56f25
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-05-17 09:51:42 +0200

    Use a List/ArrayList

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,14 +12,18 @@ public class Main {
         mutzi.setName("Mutzi");
         mutzi.setType("cat");
 
-        Pet[] myPets = new Pet[2];
-        myPets[0] = wuffi;
-        myPets[1] = mutzi;
+        // alt: Pet[] myPets = new Pet[2];
+        List<Pet> myPets = new ArrayList<>();
+        // myPets[0] = wuffi;
+        myPets.add(wuffi);
+        //myPets[1] = mutzi;
+        myPets.add(mutzi);
 
         Pet tweety = new Pet();
         tweety.setName("Tweety");
         tweety.setType("bird");
-        myPets[2] = tweety;
+        //myPets[2] = tweety;
+        myPets.add(tweety);
 
         for (Pet anyPet : myPets) {
             System.out.println("Ich füttere " + anyPet);
